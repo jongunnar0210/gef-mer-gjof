339e33bbbaac01469dbee1bbe87ee47dba10520b
Jón Gunnar Þórarinsson <jongunnar0210@gmail.com>
2020-12-30 12:12:17 +0000

    Navbar style done

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
 import hands from '../images/hendur.png';
+import logo from '../images/Logo.png';
 
 export default function FrontPage() {
   return (
     <div>
-      <div className='navBarContainer'>
-        <Navbar expand='lg' className='navbar-dark'>
-          {/* <Nav className='blackDiv'> */}
-          <Navbar.Brand href="#home"></Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' className='navbarToggler' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ml-auto'>
-              <Nav.Link href='#home'>Gerðu gjafalista</Nav.Link>
-              <Nav.Link href='#link'>Gjafahugmyndir</Nav.Link>
-              <Nav.Link href='#bla'>Innskrá | Nýskrá</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-          {/* </Nav> */}
-        </Navbar>
-      </div>
+      <Navbar expand='lg' className='navbar-dark'>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' className='navbarToggler' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ml-auto navbarItems'>
+            <Nav.Link href='#home'>Gerðu gjafalista</Nav.Link>
+            <Nav.Link href='#link'>Gjafahugmyndir</Nav.Link>
+            <Nav.Link href='#bla'>Innskrá | Nýskrá</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <Container fluid>
         <Row>
           <Col><h1>Fáðu gjöfina sem þig langar í</h1></Col>
@@ -29,7 +28,7 @@ export default function FrontPage() {
         </Row>
         <Row>
           <Col lg='11'>
-            <img src={hands} alt="hands" className='float-right' />
+            <img src={hands} alt="hands" className='float-right handsImg' />
           </Col>
         </Row>
       </Container>
